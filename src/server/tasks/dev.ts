b15b2d33802a64b0bb6a7fa176ed11389ec89b9a
NeKz <nekzor@users.noreply.github.com>
2023-06-25 23:58:33 +0200

    Add script task to set permissions for dev user

---
+++ src/server/tasks/dev.ts
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2023, NeKz
+ *
+ * SPDX-License-Identifier: MIT
+ * 
+ * This resets the permissions of the dev user.
+ * A re-login is required after execution.
+ */
+
+import "https://deno.land/std@0.177.0/dotenv/load.ts";
+import { db } from "../db.ts";
+import { UserPermissions } from "../models.ts";
+
+const { affectedRows } = await db.execute(
+  `update users
+        set permissions = ?
+      where discord_id = ?`,
+  [
+    UserPermissions.All,
+    Deno.env.get("DISCORD_USER_ID")!,
+  ],
+);
+
+console.log({ affectedRows });
