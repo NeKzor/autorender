bc65ed6eb523bc7d092fb17f8512391ce5fcd5b4
NeKz <nekzor@users.noreply.github.com>
2023-06-27 00:23:24 +0200

    Delete file loader

--- src/bot/fileloader.ts
+++
@@ -1,4 +0,0 @@
-import "file:///home/nekz/git/autorender/src/bot/./events/interactionCreate.ts#0";
-import "file:///home/nekz/git/autorender/src/bot/./events/mod.ts#0";
-import "file:///home/nekz/git/autorender/src/bot/./commands/render.ts#1";
-import "file:///home/nekz/git/autorender/src/bot/./commands/mod.ts#1";
