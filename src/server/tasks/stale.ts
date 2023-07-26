5f1d46d4ddb8a2ddb0d243bcbb6a1561bd98216a
NeKz <nekzor@users.noreply.github.com>
2023-07-27 01:55:00 +0200

    Implement stale task

---
+++ src/server/tasks/stale.ts
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2023, NeKz
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * This marks all stale videos as finished if no client
+ * was available to pick it up.
+ */
+
+import 'https://deno.land/std@0.177.0/dotenv/load.ts';
+import { db } from '../db.ts';
+import { PendingStatus, Video } from '../models.ts';
+
+const MINUTES_PAST_TO_MARK_VIDEO_AS_STALE = 2;
+
+const { affectedRows } = await db.execute<Video>(
+  `update videos
+      set pending = ?
+    where pending = ?
+      and TIMESTAMPDIFF(MINUTE, created_at, NOW()) >= ?
+    `,
+  [
+    PendingStatus.FinishedRender,
+    PendingStatus.RequiresRender,
+    MINUTES_PAST_TO_MARK_VIDEO_AS_STALE,
+  ],
+);
+
+console.log({ affectedRows });
