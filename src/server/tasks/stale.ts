c89b3eeeab0bff7d23f8fc8ef2a1f07dd124e072
NeKz <nekzor@users.noreply.github.com>
2023-07-30 12:27:10 +0200

    Add update interval to stale task

--- src/server/tasks/stale.ts
+++ src/server/tasks/stale.ts
@@ -10,21 +10,38 @@
 import 'https://deno.land/std@0.177.0/dotenv/load.ts';
 import { db } from '../db.ts';
 import { PendingStatus, Video } from '../models.ts';
+import { logger } from '../logger.ts';
 
+const STALE_VIDEOS_UPDATE_INTERVAL = 60 * 1_000;
 const MINUTES_PAST_TO_MARK_VIDEO_AS_STALE = 2;
 
-const { affectedRows } = await db.execute<Video>(
-  `update videos
+const checkStaleVideos = async () => {
+  const { affectedRows } = await db.execute<Video>(
+    `update videos
       set pending = ?
     where pending = ?
       and TIMESTAMPDIFF(MINUTE, created_at, NOW()) >= ?
       and board_changelog_id is null
     `,
-  [
-    PendingStatus.FinishedRender,
-    PendingStatus.RequiresRender,
-    MINUTES_PAST_TO_MARK_VIDEO_AS_STALE,
-  ],
-);
+    [
+      PendingStatus.FinishedRender,
+      PendingStatus.RequiresRender,
+      MINUTES_PAST_TO_MARK_VIDEO_AS_STALE,
+    ],
+  );
 
-console.log({ affectedRows });
+  logger.info('stale videos count', { affectedRows });
+};
+
+const update = async () => {
+  try {
+    logger.info('checking for stale videos');
+    await checkStaleVideos();
+  } catch (err) {
+    logger.error(err);
+  }
+};
+
+await update();
+
+setInterval(update, STALE_VIDEOS_UPDATE_INTERVAL);
