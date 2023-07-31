9edd62d207f48c5b5218008d389fe910ff3e31a7
NeKz <nekzor@users.noreply.github.com>
2023-07-31 23:59:57 +0200

    Improve board task

--- src/server/tasks/board.ts
+++ src/server/tasks/board.ts
@@ -12,6 +12,15 @@ import { AuditSource, AuditType, FixedDemoStatus, PendingStatus, RenderQuality }
 import * as uuid from 'https://deno.land/std@0.192.0/uuid/mod.ts';
 import { generateShareId, getDemoFilePath, getFixedDemoFilePath } from '../utils.ts';
 import { getDemoInfo } from '../demo.ts';
+import { logger } from '../logger.ts';
+
+const BOARD_INTEGRATION_UPDATE_INTERVAL = 60 * 1_000;
+const BOARD_INTEGRATION_STARTED_DATE = '2023-07-30';
+
+addEventListener('unhandledrejection', (ev) => {
+  ev.preventDefault();
+  logger.error('unhandledrejection', { ev });
+});
 
 const formatCmTime = (time: number) => {
   if (isNaN(time)) return '0.00';
@@ -24,14 +33,39 @@ const formatCmTime = (time: number) => {
     : `${sec}.${cs < 10 ? `0${cs}` : `${cs}`}`;
 };
 
-const extractFilenameFromHeaders = (res: Response) => {
+const fetchDemo = async (url: string) => {
+  const res = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'User-Agent': Deno.env.get('USER_AGENT')!,
+    },
+    redirect: 'manual',
+  });
+
   const location = res.headers.get('Location');
-  console.log({ location });
-  return location ? location.slice(location.lastIndexOf('/')) : 'demo.dem';
+  if (!location) {
+    logger.error({ url: res.url, headers: res.headers });
+    throw new Error('Unable to redirect without location.');
+  }
+
+  const redirect = new URL(res.url);
+  redirect.pathname = location;
+
+  const demo = await fetch(redirect.toString(), {
+    method: 'GET',
+    headers: {
+      'User-Agent': Deno.env.get('USER_AGENT')!,
+    },
+  });
+
+  return {
+    demo,
+    originalFilename: location.slice(location.lastIndexOf('/') + 1),
+  };
 };
 
-const mapsWhichAreWayTooDark: [map: string, value: number][] = [
-  ['sp_a2_bts3', 1],
+const mapsWhichAreWayTooDark: { map: string; value: number }[] = [
+  { map: 'sp_a2_bts3', value: 1 },
 ];
 
 export type ChangelogOptions =
@@ -103,7 +137,7 @@ const getChangelog = async (options?: ChangelogOptions) => {
   const query = params.toString();
 
   const url = `${BASE_API}/changelog/json?${query}`;
-  console.log(`[GET] ${url}`);
+  logger.info(`[GET] ${url}`);
 
   const res = await fetch(url, {
     headers: {
@@ -112,154 +146,160 @@ const getChangelog = async (options?: ChangelogOptions) => {
   });
 
   if (!res.ok) {
-    console.error('Failed to fetch changelog. Status:', res.statusText);
+    logger.error('Failed to fetch changelog. Status:', res.statusText);
     return null;
   }
 
   return await res.json() as ChangelogEntry[];
 };
 
-const changelog = await getChangelog({
-  endRank: 200,
-  maxDaysAgo: 1,
-  submission: 1,
-  pending: 0,
-  banned: 0,
-});
+const checkChangelogUpdates = async () => {
+  const changelog = await getChangelog({
+    endRank: 200,
+    maxDaysAgo: 1,
+    submission: 1,
+    pending: 0,
+    banned: 0,
+    chamber: '47776',
+  });
 
-if (!changelog) {
-  Deno.exit(1);
-}
-
-for (const entry of changelog) {
-  const [existingVideo] = await db.query(
-    `select 1
-       from videos
-      where board_changelog_id = ?`,
-    [entry.id],
-  );
-
-  console.log({ id: entry.id, existingVideo });
-
-  if (existingVideo) {
-    continue;
+  if (!changelog) {
+    return;
   }
 
-  let filePath = '';
-  let fixedFilePath = '';
+  for (const entry of changelog) {
+    if (entry.time_gained.slice(0, 10) < BOARD_INTEGRATION_STARTED_DATE) {
+      continue;
+    }
 
-  const fileCleanup = async () => {
-    if (filePath) {
-      try {
-        await Deno.remove(filePath);
-      } catch (err) {
-        console.error(err);
+    const [existingVideo] = await db.query(
+      `select 1
+       from videos
+      where board_changelog_id = ?`,
+      [entry.id],
+    );
+
+    logger.info({ id: entry.id, existingVideo });
+
+    if (existingVideo) {
+      continue;
+    }
+
+    let filePath = '';
+    let fixedFilePath = '';
+
+    const fileCleanup = async () => {
+      if (filePath) {
+        try {
+          await Deno.remove(filePath);
+        } catch (err) {
+          logger.error(err);
+        }
       }
-    }
 
-    if (fixedFilePath) {
-      try {
-        await Deno.remove(filePath);
-      } catch (err) {
-        console.error(err);
+      if (fixedFilePath) {
+        try {
+          await Deno.remove(filePath);
+        } catch (err) {
+          logger.error(err);
+        }
       }
-    }
-  };
+    };
 
-  try {
-    const demo = await fetch(`${BASE_API}/getDemo?id=${entry.id}`, {
-      method: 'GET',
-      headers: {
-        'User-Agent': Deno.env.get('USER_AGENT')!,
-      },
-    });
+    try {
+      const { demo, originalFilename } = await fetchDemo(`${BASE_API}/getDemo?id=${entry.id}`);
 
-    if (!demo.ok) {
-      console.error(`Unable to download demo`);
-      continue;
-    }
+      if (!demo.ok) {
+        logger.error(`Unable to download demo`);
+        continue;
+      }
 
-    const videoId = uuid.v1.generate() as string;
-    const shareId = generateShareId();
+      const videoId = uuid.v1.generate() as string;
+      const shareId = generateShareId();
 
-    filePath = getDemoFilePath(videoId);
+      filePath = getDemoFilePath(videoId);
 
-    const demoFile = await Deno.open(filePath, { write: true, create: true });
-    await demo.body?.pipeTo(demoFile.writable);
+      const demoFile = await Deno.open(filePath, { write: true, create: true });
+      await demo.body?.pipeTo(demoFile.writable);
 
-    const demoInfo = await getDemoInfo(filePath);
+      const demoInfo = await getDemoInfo(filePath);
 
-    if (demoInfo === null || typeof demoInfo === 'string') {
-      console.error('Invalid demo', demoInfo);
-      await fileCleanup();
-      continue;
-    }
+      if (demoInfo === null || typeof demoInfo === 'string') {
+        logger.error('Invalid demo', demoInfo);
+        await fileCleanup();
+        continue;
+      }
 
-    if (demoInfo.useFixedDemo) {
-      fixedFilePath = getFixedDemoFilePath(videoId);
-    }
+      if (demoInfo.useFixedDemo) {
+        fixedFilePath = getFixedDemoFilePath(videoId);
+      }
 
-    if (demoInfo.isWorkshopMap && !demoInfo.fileUrl) {
-      console.error(`Failed to resolve workshop map`);
-      await fileCleanup();
-      continue;
-    }
+      if (demoInfo.isWorkshopMap && !demoInfo.fileUrl) {
+        logger.error(`Failed to resolve workshop map`);
+        await fileCleanup();
+        continue;
+      }
 
-    const fullbrightValue = mapsWhichAreWayTooDark.find(([map]) => map === demoInfo.mapName)?.at(1);
+      const fullbrightOption = mapsWhichAreWayTooDark.find(({ map }) => map === demoInfo.mapName);
 
-    const title = `${entry.chamberName} in ${formatCmTime(parseInt(entry.score, 10))} by ${entry.player_name}`;
-    const comment = entry.note;
-    const requestedByName = null;
-    const requestedById = null;
-    const requestedInGuildId = null;
-    const requestedInGuildName = null;
-    const requestedInChannelId = null;
-    const requestedInChannelName = null;
-    const renderQuality = RenderQuality.HD_720p;
-    const renderOptions = [
-      fullbrightValue !== undefined ? `mat_fullbright ${fullbrightValue}` : null,
-    ];
-    const requiredDemoFix = demoInfo.useFixedDemo ? FixedDemoStatus.Required : FixedDemoStatus.NotRequired;
-    const demoMetadata = JSON.stringify(demoInfo.metadata);
-    const boardChangelogId = entry.id;
-    const boardProfileNumber = entry.profile_number;
-    const boardRank = entry.post_rank;
+      const title = `${entry.chamberName} in ${formatCmTime(parseInt(entry.score, 10))} by ${entry.player_name}`;
+      const comment = entry.note;
+      const requestedByName = null;
+      const requestedById = null;
+      const requestedInGuildId = null;
+      const requestedInGuildName = null;
+      const requestedInChannelId = null;
+      const requestedInChannelName = null;
+      const renderQuality = RenderQuality.HD_720p;
+      const renderOptions = [
+        fullbrightOption !== undefined ? `mat_fullbright ${fullbrightOption.value}` : null,
+      ];
+      const requiredDemoFix = demoInfo.useFixedDemo ? FixedDemoStatus.Required : FixedDemoStatus.NotRequired;
+      const demoMetadata = JSON.stringify(demoInfo.metadata);
+      const boardChangelogId = entry.id;
+      const boardProfileNumber = entry.profile_number;
+      const boardRank = entry.post_rank;
 
-    const fields = [
-      videoId,
-      shareId,
-      title,
-      comment,
-      requestedByName,
-      requestedById,
-      requestedInGuildId,
-      requestedInGuildName,
-      requestedInChannelId,
-      requestedInChannelName,
-      renderQuality,
-      renderOptions.filter((command) => command !== null).join('\n'),
-      extractFilenameFromHeaders(demo),
-      demoInfo.fileUrl,
-      demoInfo.fullMapName,
-      demoInfo.size,
-      demoInfo.mapCrc,
-      demoInfo.gameDir,
-      demoInfo.playbackTime,
-      requiredDemoFix,
-      demoInfo.tickrate,
-      demoInfo.portalScore,
-      demoInfo.timeScore,
-      demoInfo.playerName,
-      demoInfo.steamId,
-      demoMetadata,
-      boardChangelogId,
-      boardProfileNumber,
-      boardRank,
-      PendingStatus.RequiresRender,
-    ];
+      logger.info({ filename: fullbrightOption });
+      if (fullbrightOption) {
+        Deno.exit(1);
+      }
 
-    await db.execute(
-      `insert into videos (
+      const fields = [
+        videoId,
+        shareId,
+        title,
+        comment,
+        requestedByName,
+        requestedById,
+        requestedInGuildId,
+        requestedInGuildName,
+        requestedInChannelId,
+        requestedInChannelName,
+        renderQuality,
+        renderOptions.filter((command) => command !== null).join('\n'),
+        originalFilename,
+        demoInfo.fileUrl,
+        demoInfo.fullMapName,
+        demoInfo.size,
+        demoInfo.mapCrc,
+        demoInfo.gameDir,
+        demoInfo.playbackTime,
+        requiredDemoFix,
+        demoInfo.tickrate,
+        demoInfo.portalScore,
+        demoInfo.timeScore,
+        demoInfo.playerName,
+        demoInfo.steamId,
+        demoMetadata,
+        boardChangelogId,
+        boardProfileNumber,
+        boardRank,
+        PendingStatus.RequiresRender,
+      ];
+
+      await db.execute(
+        `insert into videos (
               video_id
             , share_id
             , title
@@ -291,14 +331,14 @@ for (const entry of changelog) {
             , board_rank
             , pending
           ) values (UUID_TO_BIN(?), ${new Array(fields.length - 1).fill('?').join(',')})`,
-      fields,
-    );
+        fields,
+      );
 
-    console.log('Inserted video', { video_id: videoId });
+      logger.info('Inserted video', { video_id: videoId });
 
-    try {
-      await db.execute(
-        `insert into audit_logs (
+      try {
+        await db.execute(
+          `insert into audit_logs (
                 title
               , audit_type
               , source
@@ -309,18 +349,32 @@ for (const entry of changelog) {
               , ?
               , ?
             )`,
-        [
-          `Created video ${videoId} automatically`,
-          AuditType.Info,
-          AuditSource.Server,
-          null,
-        ],
-      );
+          [
+            `Created video ${videoId} automatically`,
+            AuditType.Info,
+            AuditSource.Server,
+            null,
+          ],
+        );
+      } catch (err) {
+        logger.error(err);
+      }
     } catch (err) {
-      console.error(err);
+      logger.error(err);
+      await fileCleanup();
     }
-  } catch (err) {
-    console.error(err);
-    await fileCleanup();
   }
-}
+};
+
+const update = async () => {
+  try {
+    logger.info('checking for changelog updates');
+    await checkChangelogUpdates();
+  } catch (err) {
+    logger.error(err);
+  }
+};
+
+await update();
+
+setInterval(update, BOARD_INTEGRATION_UPDATE_INTERVAL);
