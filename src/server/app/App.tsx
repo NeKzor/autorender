127a5b2f2ae578008ae2f328a4c3a66eaea75807
NeKz <nekzor@users.noreply.github.com>
2023-08-09 00:54:26 +0200

    Add Twind CSS styling

--- src/server/app/App.tsx
+++ src/server/app/App.tsx
@@ -29,54 +29,60 @@ const getCSP = (nonce: string) => {
   return [
     `default-src 'self';`,
     `script-src 'nonce-${nonce}';`,
-    `style-src 'self' https://fonts.googleapis.com;`,
+    `style-src 'nonce-${nonce}' https://fonts.googleapis.com;`,
     `font-src 'self' https://fonts.gstatic.com;`,
     `media-src 'self' *.backblazeb2.com;`,
     `img-src 'self';`,
   ].join(' ');
 };
 
-type AppProps = {
+type HeadProps = {
   initialState: AppState;
   nonce: string;
   children?: React.ReactNode;
 };
 
-const App = ({ initialState, nonce, children }: AppProps) => {
-  const [state, dispatch] = React.useReducer(reducer, initialState);
+type BodyProps = {
+  initialState: AppState;
+  children?: React.ReactNode;
+};
+
+export const Head = ({ initialState, nonce }: HeadProps) => {
+  const [state] = React.useReducer(reducer, initialState);
   const { meta, domain } = state;
   const title = meta.title !== undefined ? `${meta.title} | ${domain}` : domain;
 
   return (
-    <html lang='en' dir='ltr'>
-      <head>
-        <meta charSet='utf-8' />
-        <meta
-          name='viewport'
-          content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no'
-        />
-        <meta http-equiv='Content-Security-Policy' content={getCSP(nonce)} />
-        <meta name='theme-color' content='#f44336' />
-        {metaNames
-          .filter((name) => meta[name] !== undefined && meta[name] !== null)
-          .map((name) => {
-            return <meta name={name} content={meta[name]} />;
-          })}
-        <title>{title}</title>
-        <link
-          rel='stylesheet'
-          href='https://fonts.googleapis.com/css?family=Roboto:300,400,500'
-        />
-      </head>
-      <body>
-        <AppStateContext.Provider value={state}>
-          <AppDispatchContext.Provider value={dispatch}>
-            {children}
-          </AppDispatchContext.Provider>
-        </AppStateContext.Provider>
-      </body>
-    </html>
+    <>
+      <meta charSet='utf-8' />
+      <meta
+        name='viewport'
+        content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no'
+      />
+      <meta http-equiv='Content-Security-Policy' content={getCSP(nonce)} />
+      <meta name='theme-color' content='#f44336' />
+      {metaNames
+        .filter((name) => meta[name] !== undefined && meta[name] !== null)
+        .map((name) => {
+          return <meta name={name} content={meta[name]} />;
+        })}
+      <title>{title}</title>
+      <link
+        rel='stylesheet'
+        href='https://fonts.googleapis.com/css?family=Roboto:300,400,500'
+      />
+    </>
   );
 };
 
-export default App;
+export const Body = ({ initialState, children }: BodyProps) => {
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+
+  return (
+    <AppStateContext.Provider value={state}>
+      <AppDispatchContext.Provider value={dispatch}>
+        {children}
+      </AppDispatchContext.Provider>
+    </AppStateContext.Provider>
+  );
+};
--- src/server/app/index.tsx
+++ src/server/app/index.tsx
@@ -5,14 +5,27 @@
  */
 
 import * as React from 'https://esm.sh/react@18.2.0';
-import { renderToReadableStream } from 'https://esm.sh/react-dom@18.2.0/server';
+import { renderToString } from 'https://esm.sh/react-dom@18.2.0/server';
 import {
   createStaticRouter,
   StaticHandlerContext,
   StaticRouterProvider,
 } from 'https://esm.sh/react-router-dom@6.11.2/server';
-import App from './App.tsx';
+import { setup } from 'https://esm.sh/twind@0.16.16';
+import { getStyleTag, virtualSheet } from 'https://esm.sh/twind@0.16.16/sheets';
+import { Body, Head } from './App.tsx';
 import { AppState } from './AppState.ts';
+
+const sheet = virtualSheet();
+
+setup({
+  theme: {
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif'],
+    },
+  },
+  sheet,
+});
 
 export const index = (
   router: ReturnType<typeof createStaticRouter>,
@@ -21,30 +34,41 @@ export const index = (
 ) => {
   const nonce = crypto.randomUUID();
 
-  return renderToReadableStream(
-    <App initialState={initialState} nonce={nonce}>
+  sheet.reset();
+
+  const head = renderToString(
+    <Head initialState={initialState} nonce={nonce}>
+    </Head>,
+  );
+
+  const body = renderToString(
+    <Body initialState={initialState}>
       <StaticRouterProvider
         router={router}
         context={context}
         nonce={nonce}
         hydrate={false}
       />
-    </App>,
-    {
-      nonce,
-      bootstrapScriptContent: Deno.env.get('HOT_RELOAD')!.toLowerCase() === 'true'
-        ? `(() => {
+    </Body>,
+  );
+
+  const styleTag = getStyleTag(sheet, { nonce });
+
+  const scriptTag = Deno.env.get('HOT_RELOAD')!.toLowerCase() === 'true'
+    ? `<script nonce="${nonce}">
+(() => {
   let ws, to, iv = null;
   const hotReload = () => {
-      ws?.close();
-      ws = new WebSocket(location.origin.replace('http', 'ws') + '/connect/__hot_reload');
-      ws.onopen = () => iv = setInterval(() => ws.send('reload?'), 500);
-      ws.onmessage = (event) => event.data === 'yes' && location.reload();
-      ws.onclose = () => clearInterval(iv) || clearTimeout(to) || (to = setTimeout(() => hotReload(), 500));
-  };
-  hotReload();
-})();`
-        : undefined,
-    },
-  );
+  ws?.close();
+  ws = new WebSocket(location.origin.replace('http', 'ws') + '/connect/__hot_reload');
+  ws.onopen = () => iv = setInterval(() => ws.send('reload?'), 500);
+  ws.onmessage = (event) => event.data === 'yes' && location.reload();
+  ws.onclose = () => clearInterval(iv) || clearTimeout(to) || (to = setTimeout(() => hotReload(), 500));
+};
+hotReload();
+})();
+</script>`
+    : '';
+
+  return `<html lang='en' dir='ltr'><head>${head}${styleTag}</head><body>${body}</body>${scriptTag}</html>`;
 };
