17bc4c0817e1095c0766f23c458c62b12461bcd8
NeKz <nekzor@users.noreply.github.com>
2023-08-13 08:17:23 +0200

    Fix video length text color

--- src/server/app/components/VideoCard.tsx
+++ src/server/app/components/VideoCard.tsx
@@ -61,6 +61,16 @@ const formatVideoLength = (videoLength: number) => {
   }`;
 };
 
+const VideoLength = ({ videoLength }: { videoLength: number }) => {
+  return (
+    <span
+      className={tw`absolute p-1 bottom-1 right-1 text-xs font-medium rounded text-white bg-gray-900 dark:bg-gray-900`}
+    >
+      {formatVideoLength(videoLength)}
+    </span>
+  );
+};
+
 export const VideoCard = ({ video }: { video: VideoCardData }) => {
   return (
     <div
@@ -79,13 +89,7 @@ export const VideoCard = ({ video }: { video: VideoCardData }) => {
                   className={tw`transition-transform duration-300 transform object-cover w-full h-full rounded-[12px]`}
                   src={video.thumbnail_url_small}
                 />
-                {video.video_length !== 0 && (
-                  <span
-                    className={tw`absolute p-1 bottom-1 right-1 text-xs font-medium rounded bg-gray-900 text-gray-50 dark:bg-gray-900 dark:text-gray-50`}
-                  >
-                    {formatVideoLength(video.video_length)}
-                  </span>
-                )}
+                {video.video_length !== null && <VideoLength videoLength={video.video_length} />}
                 {video.video_preview_url && (
                   <img
                     className={tw`absolute top-0 left-0 opacity-0 transition-opacity duration-300 transform hover:opacity-100 object-cover w-full h-full rounded-[12px]`}
@@ -106,13 +110,7 @@ export const VideoCard = ({ video }: { video: VideoCardData }) => {
                   <path d='M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z' />
                   <path d='M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z' />
                 </svg>
-                {video.video_length !== 0 && (
-                  <span
-                    className={tw`absolute p-1 bottom-1 right-1 text-xs font-medium rounded bg-gray-900 text-gray-50 dark:bg-gray-900 dark:text-gray-50`}
-                  >
-                    {formatVideoLength(video.video_length)}
-                  </span>
-                )}
+                {video.video_length !== null && <VideoLength videoLength={video.video_length} />}
               </>
             )}
         </div>
