8ae8df4566088ebb4feebbce12a28fec34750747
NeKz <nekzor@users.noreply.github.com>
2023-08-14 12:17:11 +0200

    Remove unused burger menu button

--- src/server/app/components/Navbar.tsx
+++ src/server/app/components/Navbar.tsx
@@ -157,28 +157,6 @@ const Navbar = () => {
                     </li>
                   </ul>
                 </div>
-                <button
-                  type='button'
-                  className={tw`inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600`}
-                  aria-expanded='false'
-                >
-                  <span className={tw`sr-only`}>Open main menu</span>
-                  <svg
-                    className={tw`w-5 h-5`}
-                    aria-hidden='true'
-                    xmlns='http://www.w3.org/2000/svg'
-                    fill='none'
-                    viewBox='0 0 17 14'
-                  >
-                    <path
-                      stroke='currentColor'
-                      stroke-linecap='round'
-                      stroke-linejoin='round'
-                      stroke-width='2'
-                      d='M1 1h15M1 7h15M1 13h15'
-                    />
-                  </svg>
-                </button>
               </>
             )
             : (
