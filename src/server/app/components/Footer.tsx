dc880b1fa977b0398e86f050f4db18eb9e1753d7
NeKz <nekzor@users.noreply.github.com>
2023-08-14 15:44:06 +0200

    Fix responsiveness

--- src/server/app/components/Footer.tsx
+++ src/server/app/components/Footer.tsx
@@ -10,15 +10,15 @@ import { tw } from 'https://esm.sh/twind@0.16.16';
 const Footer = () => {
   return (
     <footer className={tw`bg-white shadow dark:bg-gray-900`}>
-      <div className={tw`w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between`}>
-        <span className={tw`text-sm text-gray-500 sm:text-center dark:text-gray-400`}>
+      <div className={tw`w-full mx-auto max-w-screen-xl p-4 flex items-center justify-between`}>
+        <span className={tw`text-sm text-gray-500 text-center dark:text-gray-400`}>
           © 2023{' '}
           <a href='https://github.com/NeKzor' className={tw`hover:underline`}>
             NeKz
           </a>
         </span>
         <ul
-          className={tw`flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400`}
+          className={tw`flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 mb-0 dark:text-gray-400`}
         >
           <li>
             <a href='https://discord.gg/p2sr' target='_blank' className={tw`mr-4 hover:underline md:mr-6 `}>
