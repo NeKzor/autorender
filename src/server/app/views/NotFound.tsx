ed69e7fe21e3be2ba0d5a8d2c0eac89684ce29cc
NeKz <nekzor@users.noreply.github.com>
2023-08-14 15:44:39 +0200

    Fix responsiveness and asset link

--- src/server/app/views/NotFound.tsx
+++ src/server/app/views/NotFound.tsx
@@ -13,13 +13,13 @@ export const NotFound = () => {
 
   return (
     <>
-      <div className={tw`container flex items-center px-6 py-12 mx-auto`}>
+      <div className={tw`container md:flex items-center px-6 py-12 mx-auto`}>
         <div className={tw`wf-ull lg:w-1/2`}>
           <p className={tw`text-sm font-medium text-blue-500 dark:text-blue-400`}>
             404
           </p>
           <h1 className={tw`mt-3 text-2xl font-semibold text-gray-800 dark:text-white md:text-3xl`}>
-            Page not found :(
+            Page not found
           </h1>
           <p className={tw`mt-4 text-gray-500 dark:text-gray-400`}>
             The page you are looking for does not exist or has been moved.
@@ -27,7 +27,7 @@ export const NotFound = () => {
           <div className={tw`flex items-center mt-6 gap-x-3`}>
             <button
               id='not-found-go-back'
-              className={tw`flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700`}
+              className={tw`flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 w-auto dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700`}
             >
               <svg
                 xmlns='http://www.w3.org/2000/svg'
@@ -39,12 +39,11 @@ export const NotFound = () => {
               >
                 <path stroke-linecap='round' stroke-linejoin='round' d='M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18' />
               </svg>
-
               <span>Go back</span>
             </button>
-            <a href='/'>
+            <a href='/' tabIndex={-1}>
               <button
-                className={tw`w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600`}
+                className={tw`flex items-center w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 w-auto hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600`}
               >
                 Take me home
               </button>
@@ -54,7 +53,7 @@ export const NotFound = () => {
         <div className={tw`relative w-full mt-8 lg:w-1/2 lg:mt-0`}>
           <img
             className={tw` w-full lg:h-[32rem] h-80 md:h-96 rounded-lg object-cover`}
-            src='/images/404.jpg'
+            src='/assets/images/404.jpg'
             alt='404'
           />
         </div>
