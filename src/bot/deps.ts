cbd1986ee603f1329126120b625d2b42c6cea5b9
NeKz <nekzor@users.noreply.github.com>
2023-08-14 08:03:06 +0200

    Specify exact discordeno version

--- src/bot/deps.ts
+++ src/bot/deps.ts
@@ -4,8 +4,8 @@
  * SPDX-License-Identifier: MIT
  */
 
-export * from 'npm:@discordeno/types';
-export * from 'npm:@discordeno/utils';
-export * from 'npm:@discordeno/rest';
-export * from 'npm:@discordeno/gateway';
-export * from 'npm:@discordeno/bot';
+export * from 'npm:@discordeno/types@19.0.0-next.2d0f76e';
+export * from 'npm:@discordeno/utils@19.0.0-next.2d0f76e';
+export * from 'npm:@discordeno/rest@19.0.0-next.2d0f76e';
+export * from 'npm:@discordeno/gateway@19.0.0-next.2d0f76e';
+export * from 'npm:@discordeno/bot@19.0.0-next.2d0f76e';
