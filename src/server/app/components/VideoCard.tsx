6f82b16c7963c8b78170b55e3856417f86dec009
NeKz <nekzor@users.noreply.github.com>
2023-08-15 04:18:47 +0200

    Update missing thumbnail icon

--- src/server/app/components/VideoCard.tsx
+++ src/server/app/components/VideoCard.tsx
@@ -101,14 +101,13 @@ export const VideoCard = ({ video }: { video: VideoCardData }) => {
             : (
               <>
                 <svg
-                  className={tw`w-10 h-10 text-gray-200 dark:text-gray-600`}
+                  className={tw`w-6 h-6 text-gray-800 dark:text-white`}
                   aria-hidden='true'
                   xmlns='http://www.w3.org/2000/svg'
                   fill='currentColor'
-                  viewBox='0 0 16 20'
+                  viewBox='0 0 14 16'
                 >
-                  <path d='M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z' />
-                  <path d='M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z' />
+                  <path d='M0 .984v14.032a1 1 0 0 0 1.506.845l12.006-7.016a.974.974 0 0 0 0-1.69L1.506.139A1 1 0 0 0 0 .984Z' />
                 </svg>
                 {video.video_length !== null && <VideoLength videoLength={video.video_length} />}
               </>
