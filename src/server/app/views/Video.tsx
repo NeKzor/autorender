a53569d9d6d6037df483c5446ad7db484ec95ce3
NeKz <nekzor@users.noreply.github.com>
2023-08-20 22:17:29 +0200

    Fix download buttons

--- src/server/app/views/Video.tsx
+++ src/server/app/views/Video.tsx
@@ -334,7 +334,7 @@ export const VideoView = () => {
                     >
                       <button
                         type='button'
-                        className={tw`video-share-button flex items-center gap-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800`}
+                        className={tw`flex items-center gap-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800`}
                       >
                         <svg
                           className={tw`w-4 h-4 text-gray-800 dark:text-white`}
@@ -365,7 +365,7 @@ export const VideoView = () => {
                   >
                     <button
                       type='button'
-                      className={tw`video-share-button flex items-center gap-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800`}
+                      className={tw`flex items-center gap-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800`}
                     >
                       <svg
                         className={tw`w-4 h-4 text-gray-800 dark:text-white`}
