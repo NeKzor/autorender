7e4fca5fa09b409187227cac4df04fe181fd9a63
NeKz <nekzor@users.noreply.github.com>
2023-08-21 13:26:58 +0200

    Fix swallowed errors in rate limit middleware

--- src/server/rate_limits.ts
+++ src/server/rate_limits.ts
@@ -23,9 +23,11 @@ const buckets = {
 type OakRouterMiddleware = RouterMiddleware<any, any, Record<string, any> & AppState>;
 
 const tryConsume: (bucket: RateLimiterAbstract) => OakRouterMiddleware = (bucket) => async (ctx, next) => {
+  let consumed = false;
+
   try {
     await bucket.consume(`${ctx.request.ip}/${ctx.request.url.pathname}`, 1);
-    await next();
+    consumed = true;
   } catch (err) {
     if (err instanceof RateLimiterRes) {
       ctx.response.status = Status.TooManyRequests;
@@ -34,8 +36,12 @@ const tryConsume: (bucket: RateLimiterAbstract) => OakRouterMiddleware = (bucket
         message: STATUS_TEXT[Status.TooManyRequests],
       });
     } else {
-      ctx.throw(500);
+      throw new Error('Unknown rate limiter error', { cause: err });
     }
+  }
+
+  if (consumed) {
+    await next();
   }
 };
 
