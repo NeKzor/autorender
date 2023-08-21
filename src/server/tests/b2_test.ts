028ffdf0b187527b872e71fc43583af16ddf879b
NeKz <nekzor@users.noreply.github.com>
2023-08-21 19:18:17 +0200

    Fix broken test task

--- src/server/tests/b2_test.ts
+++ src/server/tests/b2_test.ts
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2023, NeKz
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
 import 'dotenv/load.ts';
 
 import { BackblazeClient } from '../b2.ts';
@@ -6,36 +12,37 @@ import { getVideoDownloadFilename } from '../utils.ts';
 const B2_ENABLED = Deno.env.get('B2_ENABLED')!.toLowerCase() === 'true';
 const B2_BUCKET_ID = Deno.env.get('B2_BUCKET_ID')!;
 
-if (!B2_ENABLED) {
-  console.log('Connection to b2 disabled. Skipped test.');
-  Deno.exit(0);
-}
+Deno.test('Upload test video to b2', async () => {
+  if (!B2_ENABLED) {
+    return console.log('Connection to b2 disabled. Skipped test.');
+  }
 
-const b2 = new BackblazeClient({ userAgent: Deno.env.get('USER_AGENT')! });
+  const b2 = new BackblazeClient({ userAgent: Deno.env.get('USER_AGENT')! });
 
-await b2.authorizeAccount({
-  accountId: Deno.env.get('B2_KEY_ID')!,
-  applicationKey: Deno.env.get('B2_APP_KEY')!,
+  await b2.authorizeAccount({
+    accountId: Deno.env.get('B2_KEY_ID')!,
+    applicationKey: Deno.env.get('B2_APP_KEY')!,
+  });
+
+  console.log('Connected to b2');
+
+  const fileName = crypto.randomUUID();
+  const fileContents = await Deno.readFile('./tests/videos/test.mp4');
+
+  const video = {
+    title: 'test".dem',
+    file_name: 'test".dem',
+  };
+
+  const upload = await b2.uploadFile({
+    bucketId: B2_BUCKET_ID,
+    fileName,
+    fileContents,
+    contentType: 'video/mp4',
+    contentDisposition: `attachment; filename="${encodeURIComponent(getVideoDownloadFilename(video))}"`,
+  });
+
+  const videoUrl = b2.getDownloadUrl(upload.fileName);
+
+  console.log({ videoUrl });
 });
-
-console.log('Connected to b2');
-
-const fileName = crypto.randomUUID();
-const fileContents = await Deno.readFile('./tests/videos/test.mp4');
-
-const video = {
-  title: 'test".dem',
-  file_name: 'test".dem',
-};
-
-const upload = await b2.uploadFile({
-  bucketId: B2_BUCKET_ID,
-  fileName,
-  fileContents,
-  contentType: 'video/mp4',
-  contentDisposition: `attachment; filename="${encodeURIComponent(getVideoDownloadFilename(video))}"`,
-});
-
-const videoUrl = b2.getDownloadUrl(upload.fileName);
-
-console.log({ videoUrl });
