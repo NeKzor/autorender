68b9272a5c0743567f7214afb0934dc3ac55d4f5
NeKz <nekzor@users.noreply.github.com>
2023-08-25 03:54:54 +0200

    Add missing doctype declaration

--- src/server/app/index.tsx
+++ src/server/app/index.tsx
@@ -68,5 +68,5 @@ export const index = (
   const headTag = `<head>${head}${styleTag}${themeScriptTag}${moduleScriptTag}${hotReloadScriptTag}</head>`;
   const bodyTag = `<body class="bg-gray-100 dark:bg-gray-800 dark:text-white">${body}</body>`;
 
-  return `<html lang='en' dir='ltr'>${headTag}${bodyTag}</html>`;
+  return `<!DOCTYPE html><html lang='en' dir='ltr'>${headTag}${bodyTag}</html>`;
 };
