8227243087351964efed189efe02659ca6701f9d
NeKz <nekzor@users.noreply.github.com>
2023-08-25 23:43:31 +0200

    Add default avatar for automatic renders

--- src/server/app/components/VideoRow.tsx
+++ src/server/app/components/VideoRow.tsx
@@ -88,7 +88,7 @@ export const VideoRow = ({ video }: { video: VideoRowData }) => {
                     {video.views} views | {toAgo(video.rendered_at)}
                   </div>
                 </a>
-                <div className={tw`flex items-center`}>
+                <div className={tw`flex items-center pt-2`}>
                   {video.requested_by_discord_avatar_url
                     ? (
                       <img
@@ -97,21 +97,14 @@ export const VideoRow = ({ video }: { video: VideoRowData }) => {
                       />
                     )
                     : (
-                      <svg
-                        className={tw`w-10 h-10 text-gray-200 dark:text-gray-700`}
-                        aria-hidden='true'
-                        xmlns='http://www.w3.org/2000/svg'
-                        fill='currentColor'
-                        viewBox='0 0 20 20'
-                      >
-                        <path d='M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z' />
-                      </svg>
+                      <img
+                        className={tw`w-10 h-10 text-gray-200 dark:text-gray-700 rounded-full`}
+                        src='/assets/images/portal2boards_avatar.jpg'
+                      />
                     )}
-                  {video.requested_by_name && (
-                    <div className={tw`pl-2`}>
-                      {video.requested_by_name}
-                    </div>
-                  )}
+                  <div className={tw`pl-2`}>
+                    {video.requested_by_name ?? 'Autorender'}
+                  </div>
                 </div>
                 {video.comment && (
                   <a href={`/videos/${video.share_id}`}>
