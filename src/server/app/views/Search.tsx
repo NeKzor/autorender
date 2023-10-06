e34de710b522ddafa20f021957d3a67953d8ff78
NeKz <nekzor@users.noreply.github.com>
2023-10-06 07:16:05 +0200

    Improve search Use the simplified v1 search algorithm and the new one as a fallback.

--- src/server/app/views/Search.tsx
+++ src/server/app/views/Search.tsx
@@ -7,7 +7,7 @@
 import * as React from 'react';
 import { tw } from 'twind';
 import { DataLoader, json, PageMeta, useLoaderData } from '../Routes.ts';
-import { Video } from '~/shared/models.ts';
+import { MapModel, Video } from '~/shared/models.ts';
 import { VideoRow } from '../components/VideoRow.tsx';
 import ShareModal from '../components/ShareModal.tsx';
 
@@ -51,44 +51,132 @@ export const loader: DataLoader = async ({ context }) => {
     });
   }
 
+  const searchFallback = async () => {
+    return await context.db.query<JoinedVideo>(
+      `select share_id
+            , title
+            , comment
+            , rendered_at
+            , views
+            , requested_by_id
+            , requested_by_name
+            , video_preview_url
+            , thumbnail_url_small
+            , thumbnail_url_large
+            , video_length
+            , board_changelog_id
+            , requester.username as requested_by_username
+            , requester.discord_avatar_url as requested_by_discord_avatar_url
+         from videos
+    left join users requester
+           on requester.discord_id = videos.requested_by_id
+    left join maps
+           on maps.map_id = videos.map_id
+        where video_url is not null
+          and deleted_at is null
+          and (
+               MATCH (title) AGAINST (?)
+            or maps.name sounds like ?
+            or maps.alias sounds like ?
+            or videos.demo_player_name sounds like ?)
+        limit 16`,
+      [
+        `${query}`,
+        `%${query}%`,
+        `%${query}%`,
+        `%${query}%`,
+      ],
+    );
+  };
+
+  const isWr = query.endsWith(' wr') || query.endsWith(' world record');
+  const matchedRank = / ([1-9]+)(st|nd|rd|th)$/g.exec(query)?.at(1) ?? / rank ([1-9]+)$/g.exec(query)?.at(1);
+
+  const rank = isWr ? 1 : Number(matchedRank);
+
+  const [_g1, _g2, min, sec, cs] = [.../ (([0-9]):)?([0-9]?[0-9])\.([0-9][0-9])$/g.exec(query) ?? []];
+  const time = (Number(min ?? 0) * 60 * 100) + (Number(sec ?? 0) * 100) + Number(cs ?? 0);
+
+  const words = query.split(' ');
+  const lastIndex = (isWr && words.at(-2) === 'world') || (!isNaN(rank) && words.at(-2) === 'rank')
+    ? -2
+    : isWr || matchedRank || time
+    ? -1
+    : 0;
+
+  lastIndex && words.splice(lastIndex, -lastIndex);
+
+  const mapNames = [
+    ...new Set([
+      words.join(' '),
+      words.slice(1).join(' '),
+      words.slice(0, -1).join(' '),
+    ]),
+  ].filter((name) => name.length > 0);
+
+  const [map] = mapNames.length
+    ? await context.db.query<Pick<MapModel, 'map_id' | 'alias'>>(
+      `select map_id
+              , alias
+           from maps
+          where best_time_id is not null
+            and (${mapNames.map(() => `maps.alias like ?`).join(' or ')})`,
+      mapNames,
+    )
+    : [];
+
+  if (!map) {
+    return json<Data>({
+      videos: await searchFallback(),
+    });
+  }
+
+  const joinedWords = words.join(' ').toLowerCase();
+  const mapName = map.alias.toLocaleLowerCase();
+  const playerName = joinedWords.startsWith(mapName) && !joinedWords.endsWith(mapName)
+    ? words.at(-1) ?? ''
+    : joinedWords.endsWith(mapName) && !joinedWords.startsWith(mapName)
+    ? words.at(0) ?? ''
+    : '';
+
   const videos = await context.db.query<JoinedVideo>(
     `select share_id
-          , title
-          , comment
-          , rendered_at
-          , views
-          , requested_by_id
-          , requested_by_name
-          , video_preview_url
-          , thumbnail_url_small
-          , thumbnail_url_large
-          , video_length
-          , board_changelog_id
-          , requester.username as requested_by_username
-          , requester.discord_avatar_url as requested_by_discord_avatar_url
-       from videos
-  left join users requester
-         on requester.discord_id = videos.requested_by_id
-  left join maps
-         on maps.map_id = videos.map_id
-      where video_url is not null
-        and deleted_at is null
-        and (
-             MATCH (title) AGAINST (?)
-          or maps.name sounds like ?
-          or maps.alias sounds like ?
-          or videos.demo_player_name sounds like ?)
-      limit 16`,
+            , title
+            , comment
+            , rendered_at
+            , views
+            , requested_by_id
+            , requested_by_name
+            , video_preview_url
+            , thumbnail_url_small
+            , thumbnail_url_large
+            , video_length
+            , board_changelog_id
+            , requester.username as requested_by_username
+            , requester.discord_avatar_url as requested_by_discord_avatar_url
+         from videos
+    left join users requester
+           on requester.discord_id = videos.requested_by_id
+    left join maps
+           on maps.map_id = videos.map_id
+        where board_changelog_id is not null
+          and video_url is not null
+          and videos.map_id = ?
+          ${time === 0 && isNaN(rank) && playerName.length ? `and videos.demo_player_name sounds like ?` : ''}
+          ${time !== 0 ? ' and demo_time_score = ?' : ''}
+          ${!isNaN(rank) ? ' and board_rank = ?' : ''}
+     order by videos.created_at desc
+        limit 20`,
     [
-      `${query}`,
-      `%${query}%`,
-      `%${query}%`,
-      `%${query}%`,
+      map.map_id,
+      ...(time === 0 && isNaN(rank) && playerName.length ? [playerName] : []),
+      ...(time !== 0 ? [time] : []),
+      ...(!isNaN(rank) ? [rank] : []),
     ],
   );
 
   return json<Data>({
-    videos,
+    videos: videos.length ? videos : await searchFallback(),
   });
 };
 
