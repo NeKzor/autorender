f2a1c7796ef9f91a244c8130a58bb51d733947cd
NeKz <nekzor@users.noreply.github.com>
2024-06-01 07:21:10 +0200

    Remove button margin

--- src/server/app/views/Video.tsx
+++ src/server/app/views/Video.tsx
@@ -399,7 +399,7 @@ export const VideoView = () => {
                   data-modal-toggle='delete-modal'
                   id='video-delete-button'
                   type='button'
-                  className={tw`flex items-center gap-2 text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800`}
+                  className={tw`flex items-center gap-2 text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800`}
                 >
                   <svg
                     className={tw`w-[18px] h-[18px] text-gray-800 dark:text-white`}
