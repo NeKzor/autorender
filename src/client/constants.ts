66aac70908d4c419a2c175f354b4cb46a1328943
NeKz <nekzor@users.noreply.github.com>
2024-06-27 01:41:15 +0200

    Bump version

--- src/client/constants.ts
+++ src/client/constants.ts
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: MIT
  */
 
-export const AutorenderVersion = '1.0.4';
+export const AutorenderVersion = '1.0.5';
 
 export const ReleaseTag = `client-${AutorenderVersion}`;
 
