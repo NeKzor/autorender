NeKzor/autorender

commit 61eebd117869a16b64dfaaee6c5437fa0c33e5e9

Remove demo file when inputs are parsed

src/server/tasks/processing.ts
@@ -256,21 +256,26 @@ const processVideos = async () => {
       }
     }
 
-    try {
-      const file = getDemoFilePath(video);
-      logger.info(`Parsing : ${video.share_id} : ${file}`);
+    const demoFile = getDemoFilePath(video);
 
-      const buffer = await Deno.readFile(file);
+    try {
+      logger.info(`Parsing : ${video.share_id} : ${demoFile}`);
+
+      const buffer = await Deno.readFile(demoFile);
       const demo = parser.parse(buffer);
       const inputs = getInputData(demo);
-      if (!inputs) {
-        continue;
+      if (inputs) {
+        using inputsFile = await Deno.open(getDemoInputsFilePath(video), { create: true, write: true });
+        await inputsFile.writable.getWriter().write(new Uint8Array(inputs.buffer));
       }
-
-      using inputsFile = await Deno.open(getDemoInputsFilePath(video), { create: true, write: true });
-      await inputsFile.writable.getWriter().write(new Uint8Array(inputs.buffer));
     } catch (err) {
       logger.error(`Failed to write inputs ${video.share_id}`);
+      logger.error(err);
+    }
+
+    try {
+      await Deno.remove(demoFile);
+    } catch (err) {
       logger.error(err);
     }
   }

src/server/main.ts
@@ -703,12 +703,6 @@ apiV1
               logger.error('Failed to execute board webhook for', video.video_id, ':', await webhook.text());
             }
           }
-
-          try {
-            await Deno.remove(getDemoFilePath(video));
-          } catch (err) {
-            logger.error(err);
-          }
         } else {
           type VideoUpload = Pick<
             Video,

commit c4a6c3fc91de38fffcddb30549c1a59d639b37fb

Stop deleting non-board demos Regression caused by 61eebd117869a16b64dfaaee6c5437fa0c33e5e9 Difficult to add a unit test because deletion is a delayed process.

src/server/tasks/processing.ts
@@ -46,6 +46,7 @@ type VideoSelect = Pick<
   | 'video_preview_url'
   | 'thumbnail_url_large'
   | 'thumbnail_url_small'
+  | 'board_changelog_id'
 >;
 
 const decoder = new TextDecoder();
@@ -212,6 +213,7 @@ const processVideos = async () => {
           , video_preview_url
           , thumbnail_url_large
           , thumbnail_url_small
+          , board_changelog_id
        from videos
       where processed = 0
         and pending = ?
@@ -293,10 +295,12 @@ const processVideos = async () => {
       logger.error(err);
     }
 
-    try {
-      await Deno.remove(demoFile);
-    } catch (err) {
-      logger.error(err);
+    if (video.board_changelog_id) {
+      try {
+        await Deno.remove(demoFile);
+      } catch (err) {
+        logger.error(err);
+      }
     }
   }
 };